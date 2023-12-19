47fcd1a6ec3b60ab82b9426d4c495a608f2218a4
MihaKon <konon.michal@gmail.com>
2023-12-19 20:22:20 +0100

    update seq structure

--- src/SequenceStructure.hpp
+++ src/SequenceStructure.hpp
@@ -6,8 +6,8 @@
 struct Sequence {
     int sequenceLen;
     int subSequenceLen;
-    bool positiveError;
-    bool negativeError;
+    bool positiveError = false;
+    bool negativeError = false;
     std::string sequence;
     std::string startingSubSequence;
     std::vector<std::string> subSequences;
