a8c9eca23a90ba232cad446520c2766d38eb54a4
serguppp <126479995+serguppp@users.noreply.github.com>
2024-01-04 03:14:01 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello worl!" << std::endl;
+#include "./SequenceGenerator.hpp"
+#include "./AntColonyAlgorithm.hpp"
+int main(){
+    int length = 20;
+    std::string sequence = getSequence(length);
+    
+    int subSequencesLength = 3;
+    std::vector<std::string> spectrum = getSubSequences(sequence, subSequencesLength, 0, 0);
+
+
     return 0;
 }
