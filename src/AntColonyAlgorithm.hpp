84ec4f0e391e54f5fb5502cb0b6c11a9ebc06e8f
serguppp <126479995+serguppp@users.noreply.github.com>
2024-01-08 03:58:59 +0100

    Update AntColonyAlgorithm.hpp
    create greedy heuristic algorithm

--- src/AntColonyAlgorithm.hpp
+++ src/AntColonyAlgorithm.hpp
@@ -4,20 +4,26 @@
 #include <random>
 #include <vector>
 #include <set>
+#include <optional>
+
 
 #include "./SequenceGenerator.hpp"
+#include "./LevenshteinDistanceCalc.hpp"
+#include "./LevenshteinDistanceCalc.cpp"
 
 struct Vertex;
 struct Edge;
 struct Graph;
 struct Path;
 
-std::vector<int> findBestSubpath(const Path& path);
-int ChooseInitialOligonucleotide(const std::vector<int>& V);
-int ChooseNext(const std::vector<int>& V, const int n);
-std::string ConstructForwardSolution(const Graph& G, const std::vector<std::vector<float>>& pheromones, Path& path, const std::vector<std::string>& spectrum, const int& n, const int& subSequencesLength);
-std::string ConstructBackwardSolution(const Graph& G, const std::vector<std::vector<float>>& pheromones, Path& path, const std::vector<std::string>& spectrum, const int& n, const int& subSequencesLength);
-void ApplyPheromoneUpdate();
-void ResetPheromoneValeus(std::vector<std::vector<float>>& pheromones);
-void InitializePheromoneValues(std::vector<std::vector<float>>& pheromones);
-void AntColonySBH(const Graph& graph, const int n);
+std::vector<Vertex> findBestSubpath(const Path& path, const int& n);
+Vertex chooseInitialOligo(const Graph& G);
+Vertex chooseNext(const std::vector<Vertex>& S, const Path& path, const Graph& G);
+int countPossibleSuccessors(Vertex oligo, const Path& path, const Graph& G);
+float calculateScore(const Vertex& u, const Vertex& v, const Graph& G);
+std::vector<Vertex> constructForwardSolution(const Graph& G, const std::vector<std::vector<float>>& pheromones, const int& n, const int& subSequencesLength);
+std::string constructBackwardSolution(const Graph& G, const std::vector<std::vector<float>>& pheromones, Path& path, const std::vector<std::string>& spectrum, const int& n, const int& subSequencesLength);
+void applyPheromoneUpdate();
+void resetPheromoneValeus(std::vector<std::vector<float>>& pheromones);
+void initializePheromoneValues(std::vector<std::vector<float>>& pheromones);
+void antColonySBH(const Graph& G, const int& n);
