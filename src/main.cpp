dbe68cad546718181eb2d5038c03c14e6e6472e9
serguppp <126479995+serguppp@users.noreply.github.com>
2024-01-08 03:59:28 +0100

    Update main.cpp
    test instance

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,29 @@
 #include <string>
 
 #include "./SequenceGenerator.hpp"
+#include "./SequenceGenerator.cpp"
+
 #include "./AntColonyAlgorithm.hpp"
+#include "./AntColonyAlgorithm.cpp"
+
 int main(){
-    int length = 20;
-    std::string sequence = getSequence(length);
+    int length = 7;
+    //std::string sequence = getSequence(length);
     
+    std::string sequence = "ACTCTGG";
     int subSequencesLength = 3;
     std::vector<std::string> spectrum = getSubSequences(sequence, subSequencesLength, 0, 0);
+
+    std::cout << "sequence: " << sequence << std::endl;
+
+
+    Graph G;
+    G.constructGraph(spectrum, subSequencesLength);
+    for (Edge E : G.A ){
+        std::cout << E.u.label << "->" << E.v.label << ":" << E.weight << std::endl;
+    }
+
+    antColonySBH(G, length);
 
 
     return 0;
