6d6de4e7b247b612b7055dacf88407e9bd74094d
serguppp <126479995+serguppp@users.noreply.github.com>
2024-01-16 00:46:50 +0100

    Revert "Create aco algorithm"

---
+++ src/AntColonyAlgorithm.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <set>
+
+#include "./SequenceGenerator.hpp"
+
+struct Vertex;
+struct Edge;
+struct Graph;
+struct Path;
+
+std::vector<int> findBestSubpath(const Path& path);
+int ChooseInitialOligonucleotide(const std::vector<int>& V);
+int ChooseNext(const std::vector<int>& V, const int n);
+std::string ConstructForwardSolution(const Graph& G, const std::vector<std::vector<float>>& pheromones, Path& path, const std::vector<std::string>& spectrum, const int& n, const int& subSequencesLength);
+std::string ConstructBackwardSolution(const Graph& G, const std::vector<std::vector<float>>& pheromones, Path& path, const std::vector<std::string>& spectrum, const int& n, const int& subSequencesLength);
+void ApplyPheromoneUpdate();
+void ResetPheromoneValeus(std::vector<std::vector<float>>& pheromones);
+void InitializePheromoneValues(std::vector<std::vector<float>>& pheromones);
+void AntColonySBH(const Graph& graph, const int n);
