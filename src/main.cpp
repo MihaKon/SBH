c17072f525fa6843c898420d5fda6dce6e744631
MihaKon <konon.michal@gmail.com>
2024-01-16 00:24:13 +0100

    Add Levenshtein distance calculation and output

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,29 @@
 #include "SequenceStructure.hpp"
 #include "SequenceGenerator.hpp"
 #include "BruteForceAlgorithm.hpp"
+#include "LevenshteinDistanceCalc.hpp"
 
 int main()
 {
     Sequence seq;
+    seq.positiveError = true;
+    seq.negativeError = true;
     seq.sequenceLen = 100;
     seq.subSequenceLen = 10;
     seq.sequence = getSequence(seq.sequenceLen);
-    seq.subSequences = getSubSequences(seq.sequence, seq.subSequenceLen, false, false);
-    seq.startingSubSequence = seq.sequence.substr(0, seq.subSequenceLen - 1);
+    seq.subSequences = getSubSequences(seq.sequence, seq.subSequenceLen, seq.positiveError, seq.negativeError);
+    seq.startingSubSequence = seq.sequence.substr(0, seq.subSequenceLen);
 
-    Graph graph;
-    constructGraph(seq.subSequences, graph, seq.subSequenceLen, 7);
+    std::string originalSequence = getOriginalSequence(seq.sequenceLen, seq.subSequenceLen, seq.subSequences, seq.startingSubSequence, seq.positiveError, seq.negativeError);
+
+    std::cout << "Sequence: " << seq.sequence << std::endl;
+    std::cout << "Sequence: " << originalSequence << std::endl;
+
+    int levenshtein = getLevenshteinDistance(seq.sequence, originalSequence);
+    int longer = seq.sequence.length() > originalSequence.length() ? seq.sequence.length() : originalSequence.length();
+    int percentage = (longer - levenshtein) / longer;
+    
+    std::cout << "Levenshtein distance: " << levenshtein << std::endl;
 
     return 0;
 }
