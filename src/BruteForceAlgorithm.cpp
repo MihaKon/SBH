1cea7e86f1f2eaaa49a0a18b32249cf3c1139315
MihaKon <konon.michal@gmail.com>
2024-01-15 23:19:02 +0100

    Refactor graph sorting and add missing function declarations

--- src/BruteForceAlgorithm.cpp
+++ src/BruteForceAlgorithm.cpp
@@ -5,6 +5,26 @@
 #include <map>
 #include <algorithm>
 
+struct adjNode
+{
+    std::string val;
+    int weight;
+
+    adjNode(std::string v, int w) : val(v), weight(w) {}
+
+    friend bool operator<(const adjNode& a, const adjNode& b) {
+        return a.weight > b.weight;
+    }
+};
+
+struct Graph
+{
+    std::string startingVertex;
+    std::map<std::string, std::vector<adjNode>> adjList;
+
+    Graph(std::string startingVertex) : startingVertex(startingVertex) {}
+};
+
 
 int getWeight(const std::string &str1, const std::string &str2, int subSeqLen)
 {
@@ -23,6 +43,14 @@ int getWeight(const std::string &str1, const std::string &str2, int subSeqLen)
     return commonElements;
 }
 
+void sortAdjList(Graph &graph)
+{
+    for (auto &adjList : graph.adjList)
+    {
+        std::sort(adjList.second.begin(), adjList.second.end());
+    }
+}
+
 void constructGraph(const std::vector<std::string> &subSequences, Graph &graph, int subSeqLen, int minWeightThreshold)
 {
     for (int i = 0; i < subSequences.size(); i++)
@@ -32,10 +60,12 @@ void constructGraph(const std::vector<std::string> &subSequences, Graph &graph, 
             int weight = getWeight(subSequences[i], subSequences[j], subSeqLen);
             if (weight >= minWeightThreshold)
             {
-                graph.adjList[subSequences[i]].insert(adjNode(subSequences[j], weight));
+                graph.adjList[subSequences[i]].push_back(adjNode(subSequences[j], weight));
             }
         }
     }
+
+    sortAdjList(graph);
 }
 
 std::string getOriginalSequence(
@@ -46,5 +76,21 @@ std::string getOriginalSequence(
     bool positiveErrors,
     bool negativeErrors)
 {
-    return "Test";
+    Graph graph(startingSubSeq);
+    constructGraph(subSequences, graph, subSeqLen, subSeqLen - 3);
+
+    std::string originalSequence = startingSubSeq;
+    std::string currentVertex = startingSubSeq;
+
+    for(int i = 0; i < seqLen - subSeqLen; i++)
+    {
+        if(graph.adjList[currentVertex].size() == 0)
+        {
+            break;
+        }
+        originalSequence = originalSequence + graph.adjList[currentVertex][0].val[subSeqLen - 1];
+        currentVertex = graph.adjList[currentVertex][0].val;
+    }
+    
+    return originalSequence;
 }
--- src/BruteForceAlgorithm.hpp
+++ src/BruteForceAlgorithm.hpp
@@ -2,30 +2,8 @@
 
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
-#include <algorithm>
 
-struct adjNode
-{
-    std::string val;
-    int weight;
 
-    adjNode(std::string v, int w) : val(v), weight(w) {}
-
-    friend bool operator<(const adjNode& a, const adjNode& b) {
-        return a.weight < b.weight;
-    }
-};
-
-struct Graph
-{
-    std::string startingVertex;
-    std::map<std::string, std::set<adjNode>> adjList;
-};
-
-int getWeight(const std::string &str1, const std::string &str2, int subSeqLen);
-void constructGraph(const std::vector<std::string> &subSequences, Graph &graph, int subSeqLen, int minWeightThreshold);
 std::string getOriginalSequence(
     int seqLen,
     int subSeqLen,
