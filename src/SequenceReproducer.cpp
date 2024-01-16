0d11f777e994c941ed27bdcc6bcdf6e1dae1bde6
serguppp <126479995+serguppp@users.noreply.github.com>
2024-01-16 22:20:19 +0100

    Update SequenceReproducer.cpp

--- src/SequenceReproducer.cpp
+++ src/SequenceReproducer.cpp
@@ -10,19 +10,15 @@ std::string reproduceSequence(const std::vector<std::string>& kmers, const int& 
         for (int k = 1; k <= subSeqLen; k++) {
             iLabel = kmers[i - 2][subSeqLen - k] + iLabel;
             jLabel += kmers[i - 1][k - 1];
-       //     std::cout << iLabel << ":" << jLabel << std::endl;
             if (iLabel == jLabel){
                 matchingLength=k;
             }
         }
         std::string kLabel = "";
-       // std::cout << matchingLength << std::endl;
         kLabel+=kmers[i-1].substr(matchingLength, subSeqLen-matchingLength);
         sequence += kLabel;
     }
-  //  std::cout << std::endl;
-
-    //sequence+=kmers[kmers.size()-1].substr(subSeqLen-matchingLength+1, subSeqLen-1);
+    
     return sequence;
 }
 
