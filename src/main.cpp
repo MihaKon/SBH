91397a6931a51653f754e7423555bef90f1989ec
MihaKon <konon.michal@gmail.com>
2024-01-23 01:03:45 +0100

    Refactor sequence generation and calculation

--- src/main.cpp
+++ src/main.cpp
@@ -11,84 +11,87 @@
 #include "./GreedyHeuristicAlgorithm.cpp"
 #include "./LevenshteinDistanceCalc.hpp"
 
+const int INSTANCES = 10;
+const int STARTING_SEQ_LEN = 700;
+const int STARTING_SUB_SEQ_LEN = 7;
+const int STARTING_ERROR_PERCENTAGE = 0;
+const int MAX_SEQ_LEN = 1000;
+const int MAX_SUB_SEQ_LEN = 10;
+const int MAX_ERROR_PERCENTAGE = 15;
+
 int main(int argc, char *argv[])
 {
-    // if (argc != 6)
-    // {
-    //     std::cout << "Wrong number of arguments! " << argc << std::endl;
-    //     return 1;
-    // }
-
-    // int instances = atoi(argv[1]);
-    // int SeqLen = atoi(argv[2]);
-    // int SubSeqLen = atoi(argv[3]);
-    // int positiveErrorPercentage = atoi(argv[4]);
-    // int negativeErrorPercentage = atoi(argv[5]);
-
-    int instances = 5;
-    int SeqLen = 700;
-    int SubSeqLen = 8;
-    int positiveErrorPercentage = 0;
-    int negativeErrorPercentage = 20;
 
     std::fstream result("result.txt", std::ios::out);
-    result << "\"ALGORITHM\",\"SEQ_LENGTH\",\"SUB_SEQ_LENGTH\",\"POSITIVE_ERROR_PERCENTAGE\",\"NEGATIVE_ERROR_PERCENTAGE\",\"AVG. LEVENSHTEIN_DISTANCE PER " << instances << " INSTANCES\",\"AVG. LEVENSHTEIN_PERCENTAGE PER " << instances << " INSTANCES\"" << std::endl;
+    result << "\"ALGORITHM\",\"SEQ_LENGTH\",\"SUB_SEQ_LENGTH\",\"POSITIVE_ERROR_PERCENTAGE\",\"NEGATIVE_ERROR_PERCENTAGE\",\"AVG. LEVENSHTEIN_DISTANCE PER " << INSTANCES << " INSTANCES\",\"AVG. LEVENSHTEIN_PERCENTAGE PER " << INSTANCES << " INSTANCES\"" << std::endl;
 
     std::fstream instance("instance.txt", std::ios::out);
-
 
     int totalBruteForceLevenshteinDistance = 0;
     int totalGreedyHeuristicLevenshteinDistance = 0;
     double totalBruteForceLevenshteinPercentage = 0;
     double totalGreedyHeuristicLevenshteinPercentage = 0;
 
-    for (int i = 1; i <= instances; i++)
+    for (int e = STARTING_ERROR_PERCENTAGE; e <= MAX_ERROR_PERCENTAGE; e += 5)
     {
-        instance << "SUB_SEQ_LENGTH: " << SubSeqLen << std::endl;
-        Sequence sequenceStructure(SeqLen, SubSeqLen, positiveErrorPercentage, negativeErrorPercentage);
-        sequenceStructure.sequence = getSequence(sequenceStructure.sequenceLen, &instance);
-        sequenceStructure.startingSubSequence = sequenceStructure.sequence.substr(0, sequenceStructure.subSequenceLen);
-        sequenceStructure.subSequences = getSubSequences(
-            sequenceStructure.sequence,
-            sequenceStructure.subSequenceLen,
-            sequenceStructure.positiveErrorPercentage,
-            sequenceStructure.negativeErrorPercentage,
-            &instance);
+        for (int n = STARTING_SEQ_LEN; n <= MAX_SEQ_LEN; n += 100)
+        {
+            for (int k = STARTING_SUB_SEQ_LEN; k <= MAX_SUB_SEQ_LEN; k++)
+            {
+                for (int i = 1; i <= INSTANCES; i++)
+                {
+                    instance << "SEQ_LEN: " << n << std::endl;
+                    instance << "SUB_SEQ_LENGTH: " << k << std::endl;
+                    instance << "POSITIVE_ERROR_PERCENTAGE: " << e << std::endl;
+                    instance << "NEGATIVE_ERROR_PERCENTAGE: " << e << std::endl;
+                    Sequence sequenceStructure(n, k, e, e);
+                    sequenceStructure.sequence = getSequence(sequenceStructure.sequenceLen, &instance);
+                    sequenceStructure.startingSubSequence = sequenceStructure.sequence.substr(0, sequenceStructure.subSequenceLen);
+                    sequenceStructure.subSequences = getSubSequences(
+                        sequenceStructure.sequence,
+                        sequenceStructure.subSequenceLen,
+                        sequenceStructure.positiveErrorPercentage,
+                        sequenceStructure.negativeErrorPercentage,
+                        &instance);
 
-        std::string bruteForceResult = getOriginalSequence(
-            sequenceStructure.sequenceLen,
-            sequenceStructure.subSequenceLen,
-            sequenceStructure.subSequences,
-            sequenceStructure.startingSubSequence);
+                    std::string bruteForceResult = getOriginalSequence(
+                        sequenceStructure.sequenceLen,
+                        sequenceStructure.subSequenceLen,
+                        sequenceStructure.subSequences,
+                        sequenceStructure.startingSubSequence);
 
-        int tempBruteLeven = getLevenshteinDistance(bruteForceResult, sequenceStructure.sequence);
-        totalBruteForceLevenshteinDistance += tempBruteLeven;
-        totalBruteForceLevenshteinPercentage += levenshteinPercentage(tempBruteLeven, std::max(bruteForceResult.length(), sequenceStructure.sequence.length()));
+                    instance << "BRUTE_FORCE_RESULT: " << bruteForceResult << std::endl;
 
-        Graph G;
-        G.constructGraph(sequenceStructure.subSequences, sequenceStructure.subSequenceLen);
-        std::vector<std::string> kmers = SBH(G, sequenceStructure.sequenceLen, sequenceStructure.subSequenceLen, sequenceStructure.startingSubSequence);
-        std::string greedyHeuristicResult = reproduceSequence(kmers, sequenceStructure.subSequenceLen);
+                    int tempBruteLeven = getLevenshteinDistance(bruteForceResult, sequenceStructure.sequence);
+                    totalBruteForceLevenshteinDistance += tempBruteLeven;
+                    totalBruteForceLevenshteinPercentage += levenshteinPercentage(tempBruteLeven, std::max(bruteForceResult.length(), sequenceStructure.sequence.length()));
 
-        int tempGreedyLeven = getLevenshteinDistance(greedyHeuristicResult, sequenceStructure.sequence);
-        totalGreedyHeuristicLevenshteinDistance += tempGreedyLeven;
-        totalGreedyHeuristicLevenshteinPercentage += levenshteinPercentage(tempGreedyLeven, std::max(greedyHeuristicResult.length(), sequenceStructure.sequence.length()));
+                    Graph G;
+                    G.constructGraph(sequenceStructure.subSequences, sequenceStructure.subSequenceLen);
+                    std::vector<std::string> kmers = SBH(G, sequenceStructure.sequenceLen, sequenceStructure.subSequenceLen, sequenceStructure.startingSubSequence);
+                    std::string greedyHeuristicResult = reproduceSequence(kmers, sequenceStructure.subSequenceLen);
 
-        instance << std::endl;
+                    instance << "GREEDY_HEURISTIC_RESULT: " << greedyHeuristicResult << std::endl;
+
+                    int tempGreedyLeven = getLevenshteinDistance(greedyHeuristicResult, sequenceStructure.sequence);
+                    totalGreedyHeuristicLevenshteinDistance += tempGreedyLeven;
+                    totalGreedyHeuristicLevenshteinPercentage += levenshteinPercentage(tempGreedyLeven, std::max(greedyHeuristicResult.length(), sequenceStructure.sequence.length()));
+
+                    instance << std::endl;
+                }
+
+                float avgBruteForceLevenshteinDistance = (float)totalBruteForceLevenshteinDistance / INSTANCES;
+                float avgGreedyHeuristicLevenshteinDistance = (float)totalGreedyHeuristicLevenshteinDistance / INSTANCES;
+
+                float avgBruteForceLevenshteinPercentage = (float)totalBruteForceLevenshteinPercentage / INSTANCES;
+                float avgGreedyHeuristicLevenshteinPercentage = (float)totalGreedyHeuristicLevenshteinPercentage / INSTANCES;
+
+                result << "\"BruteForce\",\"" << n << "\",\"" << k << "\",\"" << e << "\",\"" << e << "\",\"" << avgBruteForceLevenshteinDistance << "\",\"" << avgBruteForceLevenshteinPercentage << "\"" << std::endl;
+                result << "\"GreedyHeuristic\",\"" << n << "\",\"" << k << "\",\"" << e << "\",\"" << e << "\",\"" << avgGreedyHeuristicLevenshteinDistance << "\",\"" << avgGreedyHeuristicLevenshteinPercentage << "\"" << std::endl;
+            }
+        }
     }
-
-    float avgBruteForceLevenshteinDistance = (float)totalBruteForceLevenshteinDistance / instances;
-    float avgGreedyHeuristicLevenshteinDistance = (float)totalGreedyHeuristicLevenshteinDistance / instances;
-    
-    float avgBruteForceLevenshteinPercentage = (float)totalBruteForceLevenshteinPercentage / instances;
-    float avgGreedyHeuristicLevenshteinPercentage = (float)totalGreedyHeuristicLevenshteinPercentage / instances;
-
-
-    result << "\"BruteForce\",\"" << SeqLen << "\",\"" << SubSeqLen << "\",\"" << positiveErrorPercentage << "\",\"" << negativeErrorPercentage << "\",\"" << avgBruteForceLevenshteinDistance<< "\",\"" << avgBruteForceLevenshteinPercentage << "\"" << std::endl;
-    result << "\"GreedyHeuristic\",\"" << SeqLen << "\",\"" << SubSeqLen << "\",\"" << positiveErrorPercentage << "\",\"" << negativeErrorPercentage << "\",\"" << avgGreedyHeuristicLevenshteinDistance<< "\",\"" << avgGreedyHeuristicLevenshteinPercentage << "\"" << std::endl;
-
     result.close();
     instance.close();
-
     return 0;
 }
